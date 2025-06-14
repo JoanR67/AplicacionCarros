d3fde73674433c4775e29c63dac4fddd05d2ffc1
JoanR67 <joanmx794@gmail.com>
2025-06-13 20:22:17 -0600

    Add files via upload

---
+++ Models/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Carro{   
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public decimal Precio { get; set; }
+        public decimal PrecioImpuesto { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+
+}
